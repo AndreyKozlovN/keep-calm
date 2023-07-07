dceeeab8fdefcea70c94b4d8e8ac5f6f276c370c
AndreyKozlovN <kozlov205an@gmail.com>
2023-07-07 13:14:23 +0400

    настройка слайдера

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import './globals.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function RootLayout({
   children,
--- components/slider/index.jsx
+++ components/slider/index.jsx
@@ -1,43 +1,114 @@
 "use client";
 
-import React, { useRef } from "react";
-import styles from "./style.module.css";
-import { debounce } from "lodash";
+import React, { useRef, useEffect, useState } from "react";
+import Image from "next/image";
 import Slider from "react-slick";
+import styles from "./style.module.css";
+import useDetectScroll from "@smakss/react-scroll-direction";
+import { useInView } from "react-hook-inview";
 
-const HorizontalSlider = () => {
-  const sliderRef = useRef();
+const images = [
+  {
+    src: "/images/slider/1.jpg",
+  },
+  {
+    src: "/images/slider/2.jpg",
+  },
+  {
+    src: "/images/slider/3.jpg",
+  },
+  {
+    src: "/images/slider/4.jpg",
+  },
+  {
+    src: "/images/slider/5.jpg",
+  },
+  {
+    src: "/images/slider/6.jpg",
+  },
+  {
+    src: "/images/slider/7.jpg",
+  },
+  {
+    src: "/images/slider/8.jpg",
+  },
+];
+
+const SliderCarousel = () => {
+  const [oldSlide, setOldSlide] = useState(0);
+  const [activeSlide, setActiveSlide] = useState(0);
+  const [activeSlide2, setActiveSlide2] = useState(0);
+  const [componentRef, isVisibleComponentRef] = useInView({
+    threshold: 1,
+  });
+  const scrollDir = useDetectScroll({});
+  const slider = useRef();
 
   const settings = {
-    dots: true,
+    dots: false,
     arrows: false,
     infinite: false,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
+    beforeChange: (current, next) => {
+      setOldSlide(current);
+      setActiveSlide(next);
+    },
+    afterChange: (current) => setActiveSlide2(current),
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3.5,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
-  //   const onWheelSlider = debounce((e, ref) => {
-  //     if (e.deltaX > 0) {
-  //       ref.current.slickNext();
-  //     } else if (e.deltaX < 0) {
-  //       ref.current.slickPrev();
-  //     }
-  //   }, 20);
+  const gotoNext = () => {
+    slider.current.slickNext();
+  };
+
+  const gotoPrev = () => {
+    slider.current.slickPrev();
+  };
+
+  useEffect(() => {
+    if (isVisibleComponentRef) {
+      document.body.style.overflow = "hidden";
+    }
+    if (activeSlide2 <= activeSlide) {
+      document.body.style.overflow = "visible";
+    }
+  }, [activeSlide, activeSlide2, isVisibleComponentRef]);
+
+  useEffect(() => {
+    if (!isVisibleComponentRef) return;
+    if (scrollDir == "up") {
+      gotoPrev();
+    } else if (scrollDir == "down") {
+      gotoNext();
+    }
+  }, [isVisibleComponentRef, scrollDir]);
 
   return (
-    // <div className={styles.wrapper} onWheel={onWheelSlider}>
-    <Slider ref={sliderRef} {...settings}>
-      <div className={styles.item}>1</div>
-      <div className={styles.item}>2</div>
-      <div className={styles.item}>3</div>
-      <div className={styles.item}>4</div>
-      <div className={styles.item}>5</div>
-      <div className={styles.item}>6</div>
-      <div className={styles.item}>7</div>
-    </Slider>
-    // </div>
+    <section ref={componentRef}>
+      <h2 className={styles.title}>Lorem ipsum dolor sit amet</h2>
+      <div className={styles.sliderWrapper}>
+        <Slider {...settings} ref={slider}>
+          {images.map((item, index) => {
+            return (
+              <div key={item.src} className={styles.image}>
+                <Image src={item.src} alt={"slider-photo"} fill />
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
+    </section>
   );
 };
 
-export default HorizontalSlider;
+export default SliderCarousel;
