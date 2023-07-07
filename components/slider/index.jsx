3966022caf4abb8f777d4f21dfbc61e909406d0a
AndreyKozlovN <kozlov205an@gmail.com>
2023-07-07 17:05:34 +0400

    реализация тз (можно доработать

--- components/slider/index.jsx
+++ components/slider/index.jsx
@@ -4,8 +4,10 @@ import React, { useRef, useEffect, useState } from "react";
 import Image from "next/image";
 import Slider from "react-slick";
 import styles from "./style.module.css";
+import useMediaQuery from "../../hooks/useMediaQuery";
 import useDetectScroll from "@smakss/react-scroll-direction";
 import { useInView } from "react-hook-inview";
+import ReactScrollWheelHandler from "react-scroll-wheel-handler";
 
 const images = [
   {
@@ -35,78 +37,124 @@ const images = [
 ];
 
 const SliderCarousel = () => {
-  const [oldSlide, setOldSlide] = useState(0);
-  const [activeSlide, setActiveSlide] = useState(0);
-  const [activeSlide2, setActiveSlide2] = useState(0);
+  const mobile = useMediaQuery("(max-width: 768px)");
+  const scrollDir = useDetectScroll({});
   const [componentRef, isVisibleComponentRef] = useInView({
     threshold: 1,
+    rootMargin: "0px 0px 0px 0px",
   });
-  const scrollDir = useDetectScroll({});
+
   const slider = useRef();
 
   const settings = {
-    dots: false,
+    cssEase: "linear",
+    dots: true,
     arrows: false,
     infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    beforeChange: (current, next) => {
-      setOldSlide(current);
-      setActiveSlide(next);
-    },
-    afterChange: (current) => setActiveSlide2(current),
+    speed: 2000,
+    slidesToShow: 4.5,
+    slidesToScroll: 2,
     responsive: [
       {
-        breakpoint: 1400,
+        breakpoint: 1600,
         settings: {
           slidesToShow: 3.5,
           slidesToScroll: 1,
         },
       },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2.5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1.5,
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
     ],
   };
 
-  const gotoNext = () => {
+  const [endSlider, setEndSlider] = useState(false);
+  const [startSlider, setStartSlider] = useState(true);
+
+  const getLastSlideClasses = () => {
+    const parentElement = document.querySelector(".slick-track");
+    if (parentElement) {
+      const lastChildElement = parentElement.lastElementChild;
+      if (lastChildElement.classList.contains("slick-active")) {
+        setEndSlider(true);
+      } else {
+        setEndSlider(false);
+      }
+    }
+  };
+  const getFirstSlideClasses = () => {
+    const parentElement = document.querySelector(".slick-track");
+    if (parentElement) {
+      const firstChildElement = parentElement.firstElementChild;
+      if (firstChildElement.classList.contains("slick-active")) {
+        setStartSlider(true);
+      } else {
+        setStartSlider(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (isVisibleComponentRef && !mobile) {
+      if (scrollDir == "down") {
+        document.body.style.overflowY = "hidden";
+        if (endSlider) {
+          document.body.style.overflowY = "visible";
+        }
+      } else if (scrollDir == "up") {
+        document.body.style.overflowY = "hidden";
+        if (startSlider) {
+          document.body.style.overflowY = "visible";
+        }
+      }
+    }
+  }, [endSlider, isVisibleComponentRef, mobile, scrollDir, startSlider]);
+
+  const goNext = () => {
+    if (mobile) return;
     slider.current.slickNext();
+    getLastSlideClasses();
+    getFirstSlideClasses();
   };
-
-  const gotoPrev = () => {
+  const goPrev = () => {
+    if (mobile) return;
     slider.current.slickPrev();
+    getLastSlideClasses();
+    getFirstSlideClasses();
   };
-
-  useEffect(() => {
-    if (isVisibleComponentRef) {
-      document.body.style.overflow = "hidden";
-    }
-    if (activeSlide2 <= activeSlide) {
-      document.body.style.overflow = "visible";
-    }
-  }, [activeSlide, activeSlide2, isVisibleComponentRef]);
-
-  useEffect(() => {
-    if (!isVisibleComponentRef) return;
-    if (scrollDir == "up") {
-      gotoPrev();
-    } else if (scrollDir == "down") {
-      gotoNext();
-    }
-  }, [isVisibleComponentRef, scrollDir]);
 
   return (
     <section ref={componentRef}>
-      <h2 className={styles.title}>Lorem ipsum dolor sit amet</h2>
-      <div className={styles.sliderWrapper}>
-        <Slider {...settings} ref={slider}>
-          {images.map((item, index) => {
-            return (
-              <div key={item.src} className={styles.image}>
-                <Image src={item.src} alt={"slider-photo"} fill />
-              </div>
-            );
-          })}
-        </Slider>
-      </div>
+      <ReactScrollWheelHandler
+        downHandler={goNext}
+        upHandler={goPrev}
+        timeout={0}
+      >
+        <h2 className={styles.title}>Lorem ipsum dolor sit amet</h2>
+        <div className={styles.sliderWrapper}>
+          <Slider {...settings} ref={slider}>
+            {images.map((item) => {
+              return (
+                <div key={item.src} className={styles.image}>
+                  <Image src={item.src} alt={"slider-photo"} fill />
+                </div>
+              );
+            })}
+          </Slider>
+        </div>
+      </ReactScrollWheelHandler>
     </section>
   );
 };
